549c6198bc30e343cfd99e51cb4916d7b4c26431
Pradip-Dhanraj <32770009+Pradip-Dhanraj@users.noreply.github.com>
2020-06-26 22:26:20 +0530

    skills page changes

--- lib/pages/dark-theme-skills.dart
+++ lib/pages/dark-theme-skills.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:portfolio/resources/strings.dart';
 import 'package:portfolio/viewmodels/skills-viewmodel.dart';
 import 'package:provider/provider.dart';
 
 class Skills extends StatelessWidget {
-  TableRow divider() {
-    return TableRow(
-      children: [
-        SizedBox(
-          height: 30,
-        ),
-        SizedBox(
-          height: 30,
-        ),
-      ],
+
+  Widget getList(SkillsViewModel model) {
+    var item = model.skillslist.firstWhere(
+        (element) => element["title"] == model.selectedskill)["skills"];
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: item.length,
+      itemBuilder: (context, i) {
+        return Text(
+          item[i],
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 15,
+          ),
+        );
+      },
     );
   }
 
@@ -22,112 +28,41 @@ class Skills extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => SkillsViewModel(),
       child: Consumer<SkillsViewModel>(
-        builder: (context, model, child) => Table(
-          columnWidths: {
-            0: FractionColumnWidth(.3),
-          },
-          defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-          children: [
-            TableRow(
+        builder: (context, model, child) => Container(
+          height: 500,
+          child: Center(
+            child: Table(
+              columnWidths: {
+                0: FractionColumnWidth(.4),
+              },
+              defaultVerticalAlignment: TableCellVerticalAlignment.middle,
               children: [
-                //xamarin title
-                Center(
-                  child: Text(
-                    "Xamarin",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 60,
+                TableRow(
+                  children: [
+                    ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: model.skillslist.length,
+                      itemBuilder: (context, i) {
+                        return GestureDetector(
+                          child: Text(
+                            model.skillslist[i]["title"],
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: model.skillslist[i]["fontsize"],
+                            ),
+                          ),
+                          onTap: () => model.setFontSize(i, model.focusedsize),
+                        );
+                      },
                     ),
-                  ),
+                    getList(model),
+                  ],
                 ),
-                //xamarin details
-                Text(
-                  Strings.xamarinskillls,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30,
-                  ),
-                )
               ],
             ),
-            divider(),
-            TableRow(
-              children: [
-                //flutter title
-                Center(
-                  child: Text(
-                    "Flutter",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 60,
-                    ),
-                  ),
-                ),
-                //flutter details
-                Text(
-                  Strings.flutterskillls,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30,
-                  ),
-                )
-              ],
-            ),
-            divider(),
-            TableRow(
-              children: [
-                //nodejs title
-                Center(
-                  child: Text(
-                    "NodeJS",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 60,
-                    ),
-                  ),
-                ),
-                //nodejs details
-                Text(
-                  Strings.flutterskillls,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30,
-                  ),
-                )
-              ],
-            ),
-          ],
+          ),
         ),
-
-        // Row(
-        //   children: [
-        //     Flexible(
-        //       fit: FlexFit.tight,
-        //       child: Container(
-        //         color: Colors.green,
-        //         child: Center(
-        //           child: Text("data"),
-        //         ),
-        //       ),
-        //       flex: 1,
-        //     ),
-        //     Flexible(
-        //       fit: FlexFit.tight,
-        //       child: Column(
-        //         children: [
-        //           Text(
-        //             "data",
-        //             style: TextStyle(
-        //               color: Colors.white,
-        //               fontSize: 30
-        //             ),
-        //           ),
-        //         ],
-        //       ),
-        //       flex: 2,
-        //     ),
-        //   ],
-        // ),
       ),
     );
   }
--- lib/viewmodels/skills-viewmodel.dart
+++ lib/viewmodels/skills-viewmodel.dart
@@ -1,3 +1,65 @@
 import 'package:portfolio/viewmodels/base-viewmodel.dart';
 
-class SkillsViewModel extends BaseModel {}
+class SkillsViewModel extends BaseModel {
+  final double focusedsize = 100;
+  final double unfocusedsize = 30;
+  List<Map<String, dynamic>> skillslist;
+  String selectedskill;
+
+  SkillsViewModel() {
+    selectedskill = "Xamarin";
+    skillslist = [
+      {
+        "title": "Xamarin",
+        "fontsize": focusedsize,
+        "skills": [
+          "Custom renderers.",
+          "Styles & behaviors.",
+          "Native Android & iOS features.",
+          "Styles & behaviors.",
+          "Reusable components.",
+          "Knowledge of cross-platform structure.",
+          "Experience with JSON, XAML/XML, and Web Services.",
+          "State management",
+        ],
+      },
+      {
+        "title": "Flutter",
+        "fontsize": unfocusedsize,
+        "skills": [
+          " flutter Custom renderers.",
+          "Styles & behaviors.",
+          "Native Android & iOS features.",
+          "Styles & behaviors.",
+          "Reusable components.",
+          "Knowledge of cross-platform structure.",
+          "Experience with JSON, XAML/XML, and Web Services.",
+          "State management",
+        ],
+      },
+      {
+        "title": "NodeJs",
+        "fontsize": unfocusedsize,
+        "skills": [
+          "node js Custom renderers.",
+          "Styles & behaviors.",
+          "Native Android & iOS features.",
+          "Styles & behaviors.",
+          "Reusable components.",
+          "Knowledge of cross-platform structure.",
+          "Experience with JSON, XAML/XML, and Web Services.",
+          "State management",
+        ],
+      },
+    ];
+  }
+
+  void setFontSize(int index, double fontsize) {
+    for (var item in skillslist) {
+      item["fontsize"] = unfocusedsize;
+    }
+    skillslist[index]["fontsize"] = fontsize;
+    selectedskill = skillslist[index]["title"];
+    notifyListeners();
+  }
+}
